88093db901a9a45e1772271dd2520b769dc42d83
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-22 15:39:37 -0500

    Trying to add Heroku. Again.

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 var mysql= require('mysql');
-var connection= mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {mysql.createConnection({
     host:'localhost',
     user:'project2',
     password:'SecretPassword',
     database:'burgers_db'
 });
+}
 
 connection.connect(function(err){
     if(err)throw err;
