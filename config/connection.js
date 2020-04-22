55b8b2956d346bb25669dc5f453c4f3037d2726e
justin1732 <57789046+justin1732@users.noreply.github.com>
2020-04-22 15:48:18 -0500

    More heroku-ing.

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,18 @@
 var mysql= require('mysql');
 var connection;
 
-if (process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {mysql.createConnection({
+if (process.env.JAWSDB_URL) {connection= mysql.createConnection(process.env.JAWSDB_URL);} else {
+    mysql.createConnection({
     host:'localhost',
     user:'project2',
     password:'SecretPassword',
     database:'burgers_db'
 });
-}
+};
 
-connection.connect(function(err){
-    if(err)throw err;
-    console.log("connected as id: "+connection.threadid);
-});
+// connection.connect(function(err){
+//     if(err)throw err;
+//     console.log("connected as id: "+connection.threadid);
+// });
 
 module.exports = connection;
